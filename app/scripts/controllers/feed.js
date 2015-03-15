c7a118e3bf02060d7c32cb75743dc6b1a0b40445
Frederik Hanghøj Iversen <me@fredefox.eu>
2015-03-15 19:34:05 +0100

    Implement basic feed funcitonality

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -7,8 +7,21 @@
  * # MainCtrl
  * Controller of the potatoApp
  */
+// Very sad to place this in the global scope.
+var $gScope;
+var jsonFlickrFeed = function(feed) {
+  $gScope.feed = feed;
+}
 angular.module('potatoApp')
-  .controller('FeedCtrl', function ($scope) {
+  .controller('FeedCtrl', function ($scope, $http) {
+    $gScope = $scope;
+    var host, href, qs;
+    host = 'https://api.flickr.com',
+    href = '/services/feeds/photos_public.gne',
+    qs = '?tags=potato&tagmode=all&format=json';
+    $http.jsonp(host + href + qs).success(function(d) {
+        console.info("success");
+    });
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
